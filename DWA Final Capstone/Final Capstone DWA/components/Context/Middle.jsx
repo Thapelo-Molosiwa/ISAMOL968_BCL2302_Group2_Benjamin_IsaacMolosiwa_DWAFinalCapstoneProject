f653a87c0fa56216e16834dca57735d53e09964b
Thapelo-Molosiwa <0728921928p@gmail.com>
2023-08-06 12:20:14 +0200

    Added Buttons funtionalities

--- DWA Final Capstone/Final Capstone DWA/components/Context/Middle.jsx
+++ DWA Final Capstone/Final Capstone DWA/components/Context/Middle.jsx
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./middle.css";
-import { SlButton, SlCard, SlRating, SlDetails } from '@shoelace-style/shoelace/dist/react';
+import { SlCard, SlRating, SlDetails } from '@shoelace-style/shoelace/dist/react';
 
 export default function Middle() {
-  const [podcasts, setPodcasts] = useState([]);
+  const [showsData, setShowsData] = useState([]);
 
   useEffect(() => {
     fetch("https://podcast-api.netlify.app/shows")
       .then((response) => response.json())
-      .then((data) => setPodcasts(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  // Fetch episodes for each show and update the state with complete data
+      .then(async (data) => {
         const completeDataPromises = data.map(async (show) => {
           const episodesResponse = await fetch(`https://podcast-api.netlify.app/id/${show.id}`);
           const episodesData = await episodesResponse.json();
@@ -24,30 +20,26 @@ export default function Middle() {
 
         const completeData = await Promise.all(completeDataPromises);
         setShowsData(completeData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <div className="Middle-con">
       <div className="ButtonCon">
-
-        
-
+        {/* Your buttons or any other components */}
       </div>
 
-
       <div className="podcast-list">
-        {podcasts.map((podcast) => (
-          <div key={podcast.id} className="podcast-card">
+        {showsData.map((show) => (
+          <div key={show.id} className="podcast-card">
             <SlCard className="card-overview">
-              {podcast.image && <img src={podcast.image} alt={podcast.title} />}
-              <strong>{podcast.title}</strong>
+              {show.image && <img src={show.image} alt={show.title} />}
+              <strong>{show.title}</strong>
               <br />
-              <small>!!!!!</small>
+              
               <SlDetails summary="Show Description">
-                {podcast.description}
+                {show.description}
               </SlDetails>
               <SlRating></SlRating>
             </SlCard>
--- DWA Final Capstone/Final Capstone DWA/components/Header/Header.jsx
+++ DWA Final Capstone/Final Capstone DWA/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "../Header/header.css"
 import { SlButton } from '@shoelace-style/shoelace/dist/react';
+import { SlAvatar } from '@shoelace-style/shoelace/dist/react';
+
+
+
 
 export default function Header(){
 
-
+  const App = () => <SlAvatar label="User avatar" />;
   return(
 
     <div className="Header">
       <div>
-        <img src="../Images/logo_200x200 (1).pngS"/>
-        <SlButton variant="warning">Warning</SlButton>
+        
+        
       </div>
     </div>
   )
